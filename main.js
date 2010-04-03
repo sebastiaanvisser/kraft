2fdc67f3678d0fad1030b31cae06a161cba565e8
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-04-03 11:35:03 +0200

    Initial import.

---
+++ main.js
@@ -0,0 +1,4 @@
+var l = new Render.Line(100, 50, 200, 300)
+document.body.appendChild(l.elem)
+l.elem.style.background = "#aaa"
+l.render()
---
+++ src/render.js
@@ -0,0 +1,36 @@
+var Render = {}
+
+Render.Line =
+function Line (left, top, right, bottom, width)
+{
+  this.elem   = this.setupElem()
+  this.left   = left   || 0
+  this.top    = top    || 0
+  this.right  = right  || 0
+  this.bottom = bottom || 0
+  this.width  = width  || 10
+}
+
+Render.Line.prototype.setupElem =
+function setupElem ()
+{
+  var elem = document.createElement("div")
+  elem.setAttribute("class", "line")
+  elem.style.position = "absolute"
+  return elem
+}
+
+Render.Line.prototype.render =
+function render ()
+{
+  var len = Math.sqrt(Math.pow(this.right - this.left, 2) + Math.pow(this.bottom - this.top, 2))
+  var rot = Math.atan((this.bottom - this.top) / (this.right - this.left)) * 180 / Math.PI
+
+  this.elem.style.left   = ((this.left + this.right - len)        / 2) + "px"
+  this.elem.style.top    = ((this.top + this.bottom - this.width) / 2) + "px"
+  this.elem.style.width  = len                                         + "px"
+  this.elem.style.height = this.width                                  + "px"
+
+  this.elem.style["-webkit-transform"] = "rotate(" + rot + "deg)"
+}
+
