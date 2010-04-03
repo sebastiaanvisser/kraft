06c8a84484080bc9ae277404f2bfa6b5ca06d93b
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-04-03 12:49:50 +0200

    Added canvas element with some styling.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var l = new Render.Line(100, 50, 200, 300)
-document.body.appendChild(l.elem)
-l.elem.style.background = "#aaa"
+var canvas = $("#canvas")[0]
+
+var l = new Render.Line(100, 50, 200, 300, 40)
+canvas.appendChild(l.elem)
+l.elem.style.background = "#888"
 l.render()
