6ca149c4003ff9e718d2df9252a0b1aad5514b80
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-04-03 18:49:01 +0200

    Render helper function.

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
+function render (container, shape, color)
+{
+  container.appendChild(shape.elem)
+  shape.elem.style.background = color
+  shape.render()
+}
+
 var canvas = $("#canvas")[0]
 
 
-function drag (dx, dy)
-{
-  console.log(this.target, dx, dy)
-}
-
 var r = new Render.Rect(100, 50, 200, 300)
-new Draggable(canvas, r, r.elem, false, false, 10, 10, drag)
-canvas.appendChild(r.elem)
-r.elem.style.background = "#a44"
-r.render()
+new Draggable(canvas, r, r.elem, false, false, 10, 10)
+render(canvas, r, "#a44")
 
 var c = new Render.Ellipse(100, 50, 200, 300)
-new Draggable(canvas, c, c.elem, false, false, 10, 10, drag)
-canvas.appendChild(c.elem)
-c.elem.style.background = "#4a4"
-c.render()
+new Draggable(canvas, c, c.elem, false, false, 10, 10)
+render(canvas, c, "#4a4")
 
 var l = new Render.Line(100, 50, 200, 300, 10)
-new Draggable(canvas, l, l.elem, false, false, 50, 50, drag)
-canvas.appendChild(l.elem)
-l.elem.style.background = "#888"
-l.render()
+new Draggable(canvas, l, l.elem, false, false, 50, 50)
+render(canvas, l, "#000")
+
+render(canvas, l.handleA, "blue")
+// render(canvas, l.handleB, "blue")
 
