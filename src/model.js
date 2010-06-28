601438c76027f662b9a8a6a1ee1e2c631ea66de5
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-06-28 10:46:07 +0200

    Initial setup of model structure.

---
+++ src/model.js
@@ -0,0 +1,22 @@
+function Model ()
+{
+  this.def("shapes", []);
+}
+
+Model.make =
+function make ()
+{
+  var m = new Base
+  m.decorate(Model)
+  return m
+}
+
+Class(Model,
+
+  function addShape (shape)
+  {
+    this.shapes.push(shape);
+  }
+
+)
+
