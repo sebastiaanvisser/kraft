ac0d4c993e569bcaffb0a7ca44aabadbfff0eda7
Sebastiaan Visser <sfvisser@cs.uu.nl>
2011-03-20 17:59:42 +0100

    Reduced number of files to load using more SSI.

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
+// 3rd party libraries.
+<!--#include virtual="3rd/jquery-1.4.2.js" -->
+
 // CoffeeScript utilities.
 var __slice = Array.prototype.slice
 var __bind = function (fn, me) { return function bind () { return fn.apply(me, arguments); }; };
+
+// Generated JavaScript.
 <!--#exec cmd="js-shell code/gen/Compiler.js `find code/gen -name '*.js' \! -name 'Compiler.js'` -e 'js.compile();'" -->
+
+// Startup application.
 __ui_Main()
